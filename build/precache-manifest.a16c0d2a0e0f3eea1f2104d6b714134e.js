b2256d32eda146804506b978ffca5834757b4da9
karanbk-eGov <karan.bajrang@egovernments.org>
2020-03-22 14:04:17 +0530

    dep

---
+++ build/precache-manifest.a16c0d2a0e0f3eea1f2104d6b714134e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c95ba03b85db047514ed5d8c08d8843b",
+    "url": "/COVID19/index.html"
+  },
+  {
+    "revision": "a2e7bce059cb11242bbe",
+    "url": "/COVID19/static/css/main.7dae53ee.chunk.css"
+  },
+  {
+    "revision": "84991066bd3fd000f76d",
+    "url": "/COVID19/static/js/2.6c372d8c.chunk.js"
+  },
+  {
+    "revision": "a8708c1560a44a3a2f75343bf0ed33a1",
+    "url": "/COVID19/static/js/2.6c372d8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2e7bce059cb11242bbe",
+    "url": "/COVID19/static/js/main.256448b1.chunk.js"
+  },
+  {
+    "revision": "3ec8780d95108e335ac3",
+    "url": "/COVID19/static/js/runtime-main.da265baa.js"
+  },
+  {
+    "revision": "dbad07ab2347e794b0dbb5a1e8185638",
+    "url": "/COVID19/static/media/corona.dbad07ab.jpeg"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -12,17 +12,20 @@ function App() {
       <p style={{color:"white"}}>GO CORONA, CORONA GO</p>
       <div style={{display:"flex", color:"wheat",marginLeft:"14%"}} >
       <div style={{margin:"10px"}}>
-       <h1>301</h1>
+       <h1>325</h1>
       <p>Total Cases  </p>
       </div>
       <div style={{margin:"10px"}}>
-       <h1>4</h1>
+       <h1>5</h1>
       <p>DEATHS  </p>
       </div>
       <div style={{margin:"10px"}}>
        <h1>23</h1>
       <p>Recovered Cases</p>
       </div>
+      </div>
+      <div>
+    
       </div>
     
         <div className="twitterFeed">
@@ -31,6 +34,7 @@ function App() {
   screenName="WHO"
   options={{height: 400}}
 />
+<a href="https://www.mohfw.gov.in/" target="_blank">Click here for more updates</a>
 </div>
      
    
