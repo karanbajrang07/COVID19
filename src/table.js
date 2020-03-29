72c619cc7c979483351d99f06c99abfddf70c776
Karan Bajrang Bishnoi <karanbk.bpgc@gmail.com>
2020-03-29 23:06:42 +0530

    Update table.js

--- src/table.js
+++ src/table.js
@@ -7,10 +7,10 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-
+import './App.css';
 const useStyles = makeStyles({
   table: {
-    minWidth: 650,
+   // minWidth: 650,
   },
 });
 
@@ -18,64 +18,29 @@ function createData(State, cases, Cured,Deaths) {
   return { State, cases, Cured,Deaths};
 }
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
 
 class Tablee extends Component {
-state={
-    data:[],
-    rows:[]
-}
-componentDidMount() {
-
-    
-    fetch('http://covid-cases-india.azurewebsites.net/', {
-        crossDomain:true,
-        method: 'GET',
-        headers: {'Content-Type':'application/json'},
-      })
-    .then(response => response.json())
-    .then(data => this.setState({ data }))
-    .then(console.log(this.state.data))
-    //  .then(response => response.json() , console.log(response))
-     
-    //   .then(response => {
-    //     const { memes } = response.data;
-    //     this.setState({
-    //       allMemes: memes
-    //     });
-    //   });
-  }
-
 render(){
-   
+   const {rows}=this.props
   return (
-    <TableContainer component={Paper}>
+    <TableContainer className="table-c" component={Paper}>
       <Table className={useStyles.table} size="small" aria-label="a dense table">
         <TableHead>
           <TableRow>
             <TableCell>State</TableCell>
-            <TableCell align="right">Confirmed Case</TableCell>
-            <TableCell align="right">Cured</TableCell>
-            <TableCell align="right">Deaths</TableCell>
+            <TableCell >Confirmed Case</TableCell>
+            <TableCell>Cured</TableCell>
+            <TableCell >Deaths</TableCell>
             
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
             <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.State}</TableCell>
-              <TableCell align="right">{row.cases}</TableCell>
-              <TableCell align="right">{row.Cured}</TableCell>
-              <TableCell align="right">{row.Deaths}</TableCell>
+              <TableCell>{row.State}</TableCell>
+              <TableCell >{row.cases}</TableCell>
+              <TableCell >{row.Cured}</TableCell>
+              <TableCell >{row.Deaths}</TableCell>
             </TableRow>
           ))}
         </TableBody>
